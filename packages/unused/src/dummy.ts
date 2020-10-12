cd63585170b3795eee160ce82e88db40e525ca77
Jaco Greeff <jacogr@gmail.com>
2020-10-12 12:36:14 +0200

    Add dummy package to fix build (#3)

---
+++ packages/unused/src/dummy.ts
@@ -0,0 +1,10 @@
+// Copyright 2020 @polkadot/phishing authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+export interface Dummy {
+  dummy: string;
+}
+
+export default function dummy (): void {
+  console.error('unused');
+}
