f0d1b7cec1caf5bc24c72789783b5e5bf75e4cc4
Jaco Greeff <jacogr@gmail.com>
2021-01-03 11:12:06 +0100

    2021 (#41)

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-// Copyright 2020 @polkadot/phishing authors & contributors
+// Copyright 2020-2021 @polkadot/phishing authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 module.exports = require('@polkadot/dev/config/babel-config-cjs.cjs');
