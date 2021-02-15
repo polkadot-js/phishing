465e24f791fab7beaa469cad30235b4ba6c8d915
Jaco Greeff <jacogr@gmail.com>
2021-02-15 08:19:51 +0100

    Update ipfsPrep.sh

--- scripts/ipfsPrep.sh
+++ scripts/ipfsPrep.sh
@@ -3,4 +3,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
 mkdir -p build
-cp inhex.html address.json all.json known.json urlmeta.json build
+cp index.html address.json all.json known.json urlmeta.json build
