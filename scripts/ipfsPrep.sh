0fcbabc7db3713b91795c38b752e40bc2713c16f
Jaco Greeff <jacogr@gmail.com>
2021-02-15 08:15:51 +0100

    Pin content to ipfs (#101)
    * Pin content to ipfs
    
    * Update scripts/ipfsPrep.sh
    
    * Add actual packages
    
    * Update .github/workflows/push-master.yml

---
+++ scripts/ipfsPrep.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Copyright 2020-2021 @polkadot/phishing authors & contributors
+# SPDX-License-Identifier: Apache-2.0
+
+mkdir -p build
+cp inhex.html address.json all.json known.json urlmeta.json build
