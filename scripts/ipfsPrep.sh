684ebdb800bad5306e3c1a108301dd25a8ac368a
Jaco Greeff <jacogr@gmail.com>
2021-02-15 08:02:43 +0100

    Pin content to ipfs

---
+++ scripts/ipfsPrep.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Copyright 2020-2021 @polkadot/phishing authors & contributors
+# SPDX-License-Identifier: Apache-2.0
+
+mkdir -p build
+cp inhex.hsml address.json all.json known.json urlmeta.json build
---
+++ scripts/ipfsUpload.mjs
@@ -0,0 +1,65 @@
+// Copyright 2020-2021 @polkadot/phishing authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+const pinataSDK = require('@pinata/sdk');
+const cloudflare = require('dnslink-cloudflare');
+
+const SUB_DOMAIN = 'phishing';
+const DOMAIN = 'dotapps.io';
+const DST = 'build';
+const PINMETA = { name: `${SUB_DOMAIN}.${DOMAIN}` };
+
+const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_KEY);
+
+async function pin () {
+  const result = await pinata.pinFromFS(DST, { pinataMetadata: PINMETA });
+
+  console.log(`Pinned ${result.IpfsHash}`);
+
+  return result.IpfsHash;
+}
+
+async function unpin (exclude) {
+  const result = await pinata.pinList({ metadata: PINMETA, status: 'pinned' });
+
+  if (result.count > 1) {
+    const filtered = result.rows
+      .map((r) => r.ipfs_pin_hash)
+      .filter((hash) => hash !== exclude);
+
+    if (filtered.length) {
+      await Promise.all(
+        filtered.map((hash) =>
+          pinata
+            .unpin(hash)
+            .then(() => console.log(`Unpinned ${hash}`))
+            .catch(console.error)
+        )
+      );
+    }
+  }
+}
+
+async function dnslink (hash) {
+  const records = [`_dnslink.${SUB_DOMAIN}.${DOMAIN}`];
+
+  await Promise.all(records.map((record) =>
+    cloudflare(
+      { token: process.env.CF_API_TOKEN },
+      { link: `/ipfs/${hash}`, record, zone: DOMAIN }
+    )
+  ));
+
+  console.log(`Dnslink ${hash} for ${records.join(', ')}`);
+}
+
+async function main () {
+  const hash = await pin();
+
+  await dnslink(hash);
+  await unpin(hash);
+}
+
+main()
+  .catch(console.error)
+  .finally(() => process.exit());
