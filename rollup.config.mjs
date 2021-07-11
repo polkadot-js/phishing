1e81ba93ca3765b208310d624b053fe1c82e1c43
Jaco <jacogr@gmail.com>
2021-07-11 18:20:48 +0300

    Bump deps (#406)

---
+++ rollup.config.mjs
@@ -0,0 +1,32 @@
+// Copyright 2017-2021 @polkadot/api authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { createBundle } from '@polkadot/dev/config/rollup';
+
+const pkgs = [
+  '@polkadot/phishing'
+];
+
+const external = [
+  ...pkgs,
+  '@polkadot/util',
+  '@polkadot/util-crypto'
+];
+
+const entries = {};
+
+const overrides = {};
+
+export default pkgs.map((pkg) => {
+  const override = (overrides[pkg] || {});
+
+  return createBundle({
+    external,
+    pkg,
+    ...override,
+    entries: {
+      ...entries,
+      ...(override.entries || {})
+    }
+  });
+});
