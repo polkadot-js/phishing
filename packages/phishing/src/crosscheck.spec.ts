582424dbfa3e09a41edfc3ddcb59d358a228ef68
Jaco <jacogr@gmail.com>
2023-03-15 17:10:20 +0200

    Adjust types ref

--- packages/phishing/src/crosscheck.spec.ts
+++ packages/phishing/src/crosscheck.spec.ts
@@ -1,7 +1,7 @@
 // Copyright 2020-2023 @polkadot/phishing authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-/// <reference types="@polkadot/dev/node/test/node.d.ts" />
+/// <reference types="@polkadot/dev-test/node.d.ts" />
 
 import { load as yamlParse } from 'js-yaml';
 import fs from 'node:fs';
