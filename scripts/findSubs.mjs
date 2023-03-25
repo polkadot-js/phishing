7a390bf2e50472612378af958495383b4f1a68b1
Jaco <jacogr@gmail.com>
2023-03-25 17:40:31 +0200

    Add repo dev req. for Node.js (#3186)

--- scripts/findSubs.mjs
+++ scripts/findSubs.mjs
@@ -1,7 +1,7 @@
 // Copyright 2020-2023 @polkadot/phishing authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import fs from 'fs';
+import fs from 'node:fs';
 
 // muli-part domain codes and (hopefully) valid top-levels (all manual from list)
 const DOMS = [
