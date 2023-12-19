3b93f3f0b214e957d99670d71fea8a65d5e1fdf5
Jaco <jacogr@gmail.com>
2023-12-19 10:44:40 +0200

    Bump dev deps

--- packages/phishing/src/index.ts
+++ packages/phishing/src/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2020-2023 @polkadot/phishing authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import './detectPackage.js';
+import './packageDetect.js';
 
 export * from './bundle.js';
