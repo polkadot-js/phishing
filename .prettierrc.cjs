78a670487817b769a105646e40bc2f08c5d36b40
Husni <mochhusnir@gmail.com>
2026-01-02 09:08:14 +0800

    Update all.json (#5627)
    * update all.json
    
    * update all.json
    
    * fix: update headers

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,4 @@
-// Copyright 2020-2025 @polkadot/phishing authors & contributors
+// Copyright 2020-2026 @polkadot/phishing authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 module.exports = require('@polkadot/dev/config/prettier.cjs');
